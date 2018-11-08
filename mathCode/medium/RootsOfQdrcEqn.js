9e01de954fdbbf5770922e10681be8c1cf0eccc1
roshan <ouroffice143@gmail.com>
2018-11-08 11:42:37 +0530

    modified RootsOfQdrcEqn.js

--- mathCode/medium/RootsOfQdrcEqn.js
+++ mathCode/medium/RootsOfQdrcEqn.js
@@ -12,8 +12,8 @@
 }
  var a=1,b=-4,c=12;
 
- var root1 = (-b+rt(a,b,c))/(2*a)
- var root2 = (-b-rt(a,b,c))/(2*a)
+ var root1 = parseInt((-b+rt(a,b,c))/(2*a))
+ var root2 = parseInt((-b-rt(a,b,c))/(2*a))
 
 console.log(" root 1 :"+root1)
 console.log(" root 2 :"+root2)
